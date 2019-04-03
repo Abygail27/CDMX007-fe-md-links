7898c28d692acf5ac2c006493e12b0ba30640823
Abygail27 <abygaailrrocha@gmail.com>
2019-04-02 22:06:48 -0600

    getUrls para obtener las urls de un texto ejemplo!

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 const text = 'Lorem ipsum dolor sit amet, //sindresorhus.com consectetuer adipiscing http://yeoman.io elit.';
-getUrls(text);
-console.log(text);
+const urls = getUrls(text);
+console.log(urls);
